33646a70550bf9af89ac28549fe956bec16c2f45
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2022-06-17 00:57:30 +0500

    Update motive_challenge_sliced.py

--- mmdetection-master/configs/_base_/datasets/motive_challenge_sliced.py
+++ mmdetection-master/configs/_base_/datasets/motive_challenge_sliced.py
@@ -6,7 +6,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(224, 224), keep_ratio=True),
+    dict(type='Resize', img_scale=(320, 320), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(224, 224),
+        img_scale=(320, 320),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -33,15 +33,15 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'sliced/train_gt_224_02.json',
-        img_prefix=data_root + 'sliced/train_gt_images_224_02/',
+        ann_file=data_root + 'sliced/train_gt_320_02.json',
+        img_prefix=data_root + 'sliced/train_gt_images_320_02/',
         #ann_file=data_root + 'train/valid_gt.json',
         #img_prefix=data_root + 'train/train_images/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'sliced/valid_gt_224_02.json',
-        img_prefix=data_root + 'sliced/valid_gt_224_02/',
+        ann_file=data_root + 'sliced/valid_gt_320_02.json',
+        img_prefix=data_root + 'sliced/valid_gt_320_02/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
